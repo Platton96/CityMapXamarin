e88ed07b0744dd7bde115275e19c008465d65a34
Siarhei Kulbeda <Siarhei_Kulbeda@epam.com>
2018-08-07 15:01:00 +0300

    implement binding in iOS

--- CityMapXamarin.iOS/Views/MainPageView.cs
+++ CityMapXamarin.iOS/Views/MainPageView.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using CityMapXamarin.Core.ViewModels;
+using CoreGraphics;
 using Foundation;
+using MvvmCross.Binding.BindingContext;
 using MvvmCross.Platforms.Ios.Views;
 using UIKit;
 
@@ -11,6 +13,9 @@ namespace Blank.Views
 {
     public  class MainPageView : MvxViewController<MainPageViewModel>
     {
+        private UIButton _mapButton;
+        private UIView _mainView;
+        private UILabel _helloLabel;
         public MainPageView() 
         {
         }
@@ -18,6 +23,8 @@ namespace Blank.Views
         public override void ViewDidLoad()
         {
             base.ViewDidLoad();
+            InitComponents();
+            ApplyBindings();
 
             //var set = this.CreateBindingSet<TipView, TipViewModel>();
             //set.Bind(TipLabel).To(vm => vm.Tip);
@@ -29,6 +36,31 @@ namespace Blank.Views
             //{
             //    this.SubTotalTextField.ResignFirstResponder();
             //}));
+        }
+        private void InitComponents()
+        {
+            _mainView = new UIView(View.Bounds);
+            _mainView.BackgroundColor = UIColor.White;
+            View.AddSubview(_mainView);
+
+            _helloLabel = new UILabel(new CGRect(View.Frame.Width-90, 80, 80, 80));
+            _mainView.AddSubview(_helloLabel);
+
+            _mapButton = new UIButton(new CGRect(0,150,300,300));
+            _mapButton.BackgroundColor = UIColor.Green;
+            _mapButton.TitleLabel.TextColor = UIColor.Red;
+            _mapButton.Title(UIControlState.Normal);
+            _mapButton.TitleLabel.Text = "hgfgfghgf";
+            _mapButton.SetTitle("hello world", UIControlState.Normal);
+
+            _mainView.AddSubview(_mapButton);
+        }
+
+        private void ApplyBindings()
+        {
+            var bindingSet = this.CreateBindingSet<MainPageView, MainPageViewModel>();
+           bindingSet.Bind(_helloLabel).For(b => b.Text).To(vm => vm.Hello);
+            bindingSet.Apply();
         }
     }
 }
