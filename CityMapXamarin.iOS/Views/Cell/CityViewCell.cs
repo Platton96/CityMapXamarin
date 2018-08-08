cfa6626825a50782f364525b0c99e4e20535d01c
Siarhei Kulbeda <Siarhei_Kulbeda@epam.com>
2018-08-08 16:26:48 +0300

    add cell

---
+++ CityMapXamarin.iOS/Views/Cell/CityViewCell.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+using Foundation;
+using MvvmCross.Platforms.Ios.Binding.Views;
+using UIKit;
+
+namespace Blank.Views.Cell
+{
+    public partial class CityViewCell : MvxTableViewCell
+    {
+        public static readonly NSString Key = new NSString("CityViewCell");
+        public static readonly UINib Nib;
+
+        static CityViewCell()
+        {
+            Nib = UINib.FromName("CityViewCell", NSBundle.MainBundle);
+        }
+
+        protected CityViewCell(IntPtr handle) : base(handle)
+        {
+            // Note: this .ctor should not contain any initialization logic.
+        }
+    }
+}
